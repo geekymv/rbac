aa615118c1db1e5828bd2bb631b6441e14658e61
geekymv <ym2011678@foxmail.com>
2018-05-31 15:48:40 +0800

    添加fastjson支持

---
+++ src/main/java/com/ccdc/rbac/config/HttpMessageConverterConfig.java
@@ -0,0 +1,27 @@
+package com.ccdc.rbac.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+
+@Configuration
+public class HttpMessageConverterConfig {
+
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverter() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteDateUseDateFormat
+        );
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastConverter;
+
+        return new HttpMessageConverters(converter);
+    }
+}

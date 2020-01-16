5d82cd4605ad1dbf0aec8dc23163939d565afafe
geekymv <ym2011678@foxmail.com>
2020-01-16 15:35:20 +0800

    密码加密

--- rbac-api/src/main/java/com/rbac/service/UserService.java
+++ rbac-api/src/main/java/com/rbac/service/UserService.java
@@ -5,8 +5,25 @@ import com.rbac.model.User;
 
 public interface UserService {
 
+    /**
+     * 根据用户id获取用户基本信息
+     * @param userId
+     * @return
+     */
     User getUserInfo(String userId);
 
+    /**
+     * 新增用户（帐号account 不能修改且唯一）
+     * @param user
+     * @return
+     */
     User add(User user);
 
+    /**
+     * 验证密码
+     * @param encPass 加密后密码
+     * @param rawPass 原始密码
+     * @return
+     */
+    boolean isPasswordValid(String encPass, String rawPass, String salt);
 }
--- rbac-service/src/main/java/com/rbac/service/impl/UserServiceImpl.java
+++ rbac-service/src/main/java/com/rbac/service/impl/UserServiceImpl.java
@@ -2,9 +2,10 @@ package com.rbac.service.impl;
 
 import com.rbac.base.BaseService;
 import com.rbac.model.User;
-import com.rbac.service.UserService;
 import com.rbac.repository.UserMapper;
+import com.rbac.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -24,7 +25,18 @@ public class UserServiceImpl extends BaseService<UserMapper, User> implements Us
 
     @Override
     public User add(User user) {
-       userMapper.insert(user);
-       return user;
+        // 密码加密
+        Md5PasswordEncoder passwordEncoder = new Md5PasswordEncoder();
+        String password = passwordEncoder.encodePassword(user.getPassword(), user.getAccount());
+        user.setPassword(password);
+
+        userMapper.insert(user);
+        return user;
+    }
+
+    @Override
+    public boolean isPasswordValid(String encPass, String rawPass, String salt) {
+        Md5PasswordEncoder passwordEncoder = new Md5PasswordEncoder();
+        return passwordEncoder.isPasswordValid(encPass, rawPass, salt);
     }
 }

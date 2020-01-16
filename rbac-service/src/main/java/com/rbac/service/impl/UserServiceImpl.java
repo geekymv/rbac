b95a19412c6e4039041bba72c11e17351603718c
geekymv <ym2011678@foxmail.com>
2020-01-16 15:53:45 +0800

    创建用户

--- rbac-service/src/main/java/com/rbac/service/impl/UserServiceImpl.java
+++ rbac-service/src/main/java/com/rbac/service/impl/UserServiceImpl.java
@@ -1,14 +1,17 @@
 package com.rbac.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.rbac.base.BaseService;
 import com.rbac.model.User;
 import com.rbac.repository.UserMapper;
 import com.rbac.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 @Slf4j
 @Service
@@ -25,11 +28,32 @@ public class UserServiceImpl extends BaseService<UserMapper, User> implements Us
 
     @Override
     public User add(User user) {
-        // 密码加密
-        Md5PasswordEncoder passwordEncoder = new Md5PasswordEncoder();
-        String password = passwordEncoder.encodePassword(user.getPassword(), user.getAccount());
-        user.setPassword(password);
+        final String account = user.getAccount();
+        final String password = user.getPassword();
+        if(StringUtils.isBlank(account)) {
+            throw new IllegalArgumentException("参数account不能为空");
+        }
+        if(StringUtils.isBlank(password)) {
+            throw new IllegalArgumentException("参数password不能为空");
+        }
 
+        // 验证帐号是否已存在
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        userQueryWrapper.eq("account", account);
+        Integer userCnt = userMapper.selectCount(userQueryWrapper);
+        if(userCnt != null && userCnt > 0) {
+            throw new RuntimeException("帐号"+account+"已存在，无法再次创建");
+        }
+
+        // 将用户输入的密码加密
+        Md5PasswordEncoder passwordEncoder = new Md5PasswordEncoder();
+        String encPassword = passwordEncoder.encodePassword(password, account);
+        user.setPassword(encPassword);
+
+        // 设置创建时间
+        user.setCreateAt(new Date());
+
+        // 保存用户信息
         userMapper.insert(user);
         return user;
     }
